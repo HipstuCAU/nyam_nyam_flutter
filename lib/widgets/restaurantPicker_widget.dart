d66f2469c321b99e198e63133d8eee2159edb8e4
Nick(Seungyun Kim) <sy5072@naver.com>
2023-03-06 20:49:03 +0900

    [feat] restaurant picker 분리 (#1)

---
+++ lib/widgets/restaurantPicker_widget.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:nyam_nyam_flutter/extensions/colors+.dart';
+import 'package:nyam_nyam_flutter/models/customType.dart';
+
+class RestaurantPicker extends StatefulWidget {
+  RestaurantPicker({
+    super.key,
+    required this.entryPoint,
+    required this.isSelectedRestaurant,
+    required this.seoulRestaurantName,
+    required this.ansungRestaurantName,
+  });
+
+  final CampusType entryPoint;
+  List<bool> isSelectedRestaurant;
+  final List<String> seoulRestaurantName;
+  final List<String> ansungRestaurantName;
+
+  @override
+  State<RestaurantPicker> createState() => _RestaurantPickerState();
+}
+
+class _RestaurantPickerState extends State<RestaurantPicker> {
+  void touchUpToInsideToSelectRestaurant(int index) {
+    setState(() {
+      widget.isSelectedRestaurant = [
+        false,
+        false,
+        false,
+        false,
+        false,
+      ];
+      widget.isSelectedRestaurant[index] = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 14,
+      ),
+      child: SizedBox(
+        height: 34,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: widget.entryPoint == CampusType.Seoul
+              ? SeoulRestaurantType.values.length
+              : AnsungRestaurantType.values.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(
+                left: 20,
+                // right: 10,
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      offset: const Offset(0, 2),
+                      blurRadius: 3,
+                      spreadRadius: 1,
+                      color: Colors.black.withOpacity(0.1),
+                    )
+                  ],
+                  color: widget.isSelectedRestaurant[index]
+                      ? NyamColors.cauBlue
+                      : Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: GestureDetector(
+                  onTap: () {
+                    touchUpToInsideToSelectRestaurant(index);
+                  },
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child: Text(
+                      widget.entryPoint == CampusType.Seoul
+                          ? widget.seoulRestaurantName[index]
+                          : widget.ansungRestaurantName[index],
+                      style: TextStyle(
+                        color: widget.isSelectedRestaurant[index]
+                            ? Colors.white
+                            : Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
