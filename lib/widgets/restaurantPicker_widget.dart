270279a6794a99c0a678508cb7db8f0c6b32581d
Nick(Seungyun Kim) <sy5072@naver.com>
2023-03-08 03:37:12 +0900

    [feat] restaurant picker scroll animation (#1)

--- lib/widgets/restaurantPicker_widget.dart
+++ lib/widgets/restaurantPicker_widget.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:nyam_nyam_flutter/extensions/colors+.dart';
 import 'package:nyam_nyam_flutter/models/customType.dart';
 import 'package:nyam_nyam_flutter/screens/home_screen.dart';
+import 'package:scroll_to_index/scroll_to_index.dart';
 
 class RestaurantPicker extends StatefulWidget {
-  const RestaurantPicker({
+  RestaurantPicker({
     super.key,
     required this.seoulRestaurantName,
     required this.ansungRestaurantName,
@@ -12,6 +13,7 @@ class RestaurantPicker extends StatefulWidget {
 
   final List<String> seoulRestaurantName;
   final List<String> ansungRestaurantName;
+  AutoScrollController autoScrollController = AutoScrollController();
 
   @override
   State<RestaurantPicker> createState() => _RestaurantPickerState();
@@ -29,6 +31,11 @@ class _RestaurantPickerState extends State<RestaurantPicker> {
       ];
       HomeScreen.isSelectedRestaurant[index] = true;
       HomeScreen.pageController.jumpToPage(index);
+      widget.autoScrollController.animateTo(
+        index * 100,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeIn,
+      );
     });
   }
 
@@ -41,53 +48,60 @@ class _RestaurantPickerState extends State<RestaurantPicker> {
       child: SizedBox(
         height: 50,
         child: ListView.builder(
+          controller: widget.autoScrollController,
           scrollDirection: Axis.horizontal,
           itemCount: HomeScreen.entryPoint == CampusType.seoul
               ? widget.seoulRestaurantName.length
               : widget.ansungRestaurantName.length,
           itemBuilder: (context, index) {
-            return Padding(
-              padding: EdgeInsets.only(
-                left: index == 0 ? 20 : 10,
-                top: 5,
-                bottom: 10,
-                right: index == widget.seoulRestaurantName.length - 1 ? 20 : 0,
-              ),
-              child: Container(
-                height: 30,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      offset: const Offset(0, 0),
-                      blurRadius: 3,
-                      spreadRadius: 1,
-                      color: Colors.black.withOpacity(0.1),
-                    )
-                  ],
-                  color: HomeScreen.isSelectedRestaurant[index]
-                      ? NyamColors.cauBlue
-                      : Colors.white,
-                  borderRadius: BorderRadius.circular(30),
+            return AutoScrollTag(
+              key: ValueKey(index),
+              controller: widget.autoScrollController,
+              index: index,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: index == 0 ? 20 : 10,
+                  top: 5,
+                  bottom: 10,
+                  right:
+                      index == widget.seoulRestaurantName.length - 1 ? 20 : 0,
                 ),
-                clipBehavior: Clip.none,
-                child: GestureDetector(
-                  onTap: () {
-                    touchUpToInsideToSelectRestaurant(index);
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 5,
-                    ),
-                    child: Text(
-                      HomeScreen.entryPoint == CampusType.seoul
-                          ? widget.seoulRestaurantName[index]
-                          : widget.ansungRestaurantName[index],
-                      style: TextStyle(
-                        color: HomeScreen.isSelectedRestaurant[index]
-                            ? Colors.white
-                            : Colors.black,
-                        fontSize: 16,
+                child: Container(
+                  height: 30,
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        offset: const Offset(0, 0),
+                        blurRadius: 3,
+                        spreadRadius: 1,
+                        color: Colors.black.withOpacity(0.1),
+                      )
+                    ],
+                    color: HomeScreen.isSelectedRestaurant[index]
+                        ? NyamColors.cauBlue
+                        : Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  clipBehavior: Clip.none,
+                  child: GestureDetector(
+                    onTap: () {
+                      touchUpToInsideToSelectRestaurant(index);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      child: Text(
+                        HomeScreen.entryPoint == CampusType.seoul
+                            ? widget.seoulRestaurantName[index]
+                            : widget.ansungRestaurantName[index],
+                        style: TextStyle(
+                          color: HomeScreen.isSelectedRestaurant[index]
+                              ? Colors.white
+                              : Colors.black,
+                          fontSize: 16,
+                        ),
                       ),
                     ),
                   ),
