567cc71b6d012854c43b37ae347d52d47680933f
Nick(Seungyun Kim) <sy5072@naver.com>
2023-03-09 16:05:54 +0900

    [feat] favorite campus picekr 구현 (#3)

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -21,9 +21,57 @@ class SettingScreen extends StatelessWidget {
         ),
         elevation: 0,
       ),
-      body: Row(
-        children: const [
-          Text("기본 캠퍼스 설정"),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 14,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "기본 캠퍼스 설정",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "서울",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 3, left: 8),
+                          child: Icon(
+                            Icons.arrow_forward_ios_rounded,
+                            size: 16,
+                            color: NyamColors.customGrey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
         ],
       ),
     );
